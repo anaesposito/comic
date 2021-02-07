ecf6e3b7276733d2fa845788ccaad788cf4662fe
anaesposito <anaclaraesposito@gmail.com>
2021-02-07 20:19:25 -0300

    adding style

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const cardsLayout = document.querySelector(".cards-layout");
+
 fetch(
   "https://gateway.marvel.com/v1/public/comics?apikey=5b28d7dfab933cb0faf686ed9e76a30a"
 )
   .then((res) => {
     return res.json();
   })
-  .then((data) => {
-    console.log(data);
+  .then((info) => {
+    // console.log(data.data.results);
+    info.data.results.map((comic) => {
+      console.log(info.data.results);
+      // console.log(comic.characters);
+      cardsLayout.innerHTML += `<article>
+          <div class="comic-article"><img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt=""></div>
+          <p></p>
+      </article>`;
+    });
   });
