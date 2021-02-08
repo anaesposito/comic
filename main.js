181fe97e48fa6965e418b295ecd4a145136096c2
anaesposito <anaclaraesposito@gmail.com>
2021-02-08 18:34:01 -0300

    cards styled and notfound.png working fine

--- main.js
+++ main.js
@@ -12,8 +12,19 @@ fetch(
       console.log(info.data.results);
       // console.log(comic.characters);
       cardsLayout.innerHTML += `<article>
-          <div class="comic-article"><img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt=""></div>
+          <div class="comic-article"><img class="comic-thumbnail" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt=""></div>
           <p></p>
       </article>`;
+    });
+    const thumbnails = document.querySelectorAll(".comic-thumbnail");
+
+    thumbnails.forEach((comic) => {
+      // console.log(comic.src);
+      if (
+        comic.src ==
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        comic.src = "img/notfound.png";
+      }
     });
   });
