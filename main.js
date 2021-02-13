03aa65cd7263f90b84c866baa7d1350e3e00728e
anaesposito <anaclaraesposito@gmail.com>
2021-02-12 21:44:36 -0300

    onsearch working

--- main.js
+++ main.js
@@ -7,25 +7,7 @@ fetch(
     return res.json();
   })
   .then((info) => {
-    console.log(info);
-    info.data.results.map((comic) => {
-      // console.log(info.data.results);
-
-      cardsLayout.innerHTML += `<article class="comic-article"> 
-          <img class="comic-thumbnail" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="">
-          <p class="comic-title">${comic.title}</p>
-      </article>`;
-    });
-    const thumbnails = document.querySelectorAll(".comic-thumbnail");
-
-    thumbnails.forEach((comic) => {
-      if (
-        comic.src ==
-        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-      ) {
-        comic.src = "img/notfound.png";
-      }
-    });
+    cardsGenerator(info);
   });
 // ----------------------  End of Cards Generations
 
@@ -37,23 +19,37 @@ console.log(searchInput);
 const onSearch = () => {
   searchInput.oninput = () => {
     const searchValue = searchInput.value.toLowerCase();
-    console.log(searchValue);
+    // console.log(searchValue);
     fetch(
-      "https://gateway.marvel.com/v1/public/comics?apikey=5b28d7dfab933cb0faf686ed9e76a30a"
+      `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${searchValue}&apikey=5b28d7dfab933cb0faf686ed9e76a30a`
     )
       .then((res) => {
         return res.json();
       })
       .then((info) => {
-        info.data.results.map((comic, index, arr) => {
-          const title = comic.title;
-          if (searchValue.includes(title)) {
-            console.log("ENCONTRADO", title);
-          } else {
-            console.log("nada", title);
-          }
-        });
+        cardsGenerator(info);
       });
   };
 };
+
 onSearch();
+//
+const cardsGenerator = (info) => {
+  cardsLayout.innerHTML = "";
+  info.data.results.map((comic) => {
+    cardsLayout.innerHTML += `<article class="comic-article"> 
+      <img class="comic-thumbnail" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="">
+      <p class="comic-title">${comic.title}</p>
+  </article>`;
+  });
+  const thumbnails = document.querySelectorAll(".comic-thumbnail");
+
+  thumbnails.forEach((comic) => {
+    if (
+      comic.src ==
+      "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    ) {
+      comic.src = "img/notfound.png";
+    }
+  });
+};
